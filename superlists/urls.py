e974114f126716f2bfe625f6f64953c06154ae04
Iorvit <iiorvit@gmail.com>
2023-07-14 17:47:48 +0400

    new URl, view and template to display lists

--- superlists/urls.py
+++ superlists/urls.py
@@ -18,7 +18,8 @@ from django.conf.urls import url
 # from django.contrib import admin
 from lists import views
 
+
 urlpatterns = [
-    # url(r'^admin/', admin.site.urls),
     url(r"^$", views.home_page, name="home"),
+    url(r"^lists/the-only-list-in-the-world/$", views.view_list, name="view_list"),
 ]
